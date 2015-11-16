37012d4f44590d0a187e8ef28f63c1b1e611ac44
Christoph Boehner <cboehner@team23.de>
2015-11-16 17:12:15 +0100

    Add default view for contacts

--- ade25/contacts/browser/contact.py
+++ ade25/contacts/browser/contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-"""Module providing views for the folderish content page type"""
+"""Module providing views for contact page type"""
 from Products.Five.browser import BrowserView
 
 
 class ContactView(BrowserView):
-    """ Folderish content page default view """
+    """ Folderish contact item default view """
