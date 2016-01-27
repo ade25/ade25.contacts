3aa31b337018ed108e164da4e5d763071b604453
Christoph Böhner-Figas <boehner@team23.de>
2016-01-27 16:56:18 +0100

    Add contact base view

--- ade25/contacts/browser/contact.py
+++ ade25/contacts/browser/contact.py
@@ -10,6 +10,28 @@ from ade25.contacts.interfaces import IContactImagesTool
 class ContactView(BrowserView):
     """ Folderish contact item default view """
 
+    def has_image(self):
+        context = aq_inner(self.context)
+        try:
+            lead_img = context.image
+        except AttributeError:
+            lead_img = None
+        if lead_img is not None:
+            return True
+        return False
+
+    def has_address_info(self):
+        context = aq_inner(self.context)
+        if context.address or context.city:
+            return True
+        return False
+
+    def has_position_info(self):
+        context = aq_inner(self.context)
+        if context.position or context.department:
+            return True
+        return False
+
     def get_image_data(self, uuid):
         tool = getUtility(IContactImagesTool)
         return tool.create(uuid)
