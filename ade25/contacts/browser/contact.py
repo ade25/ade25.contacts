b78bdd9672b1f06027010d759e6827cada6dd335
Christoph Böhner-Figas <boehner@team23.de>
2016-02-22 14:27:57 +0100

    Provide alternative embeddable contact element view

--- ade25/contacts/browser/contact.py
+++ ade25/contacts/browser/contact.py
@@ -70,3 +70,33 @@ class ContactCardView(BrowserView):
     def get_image_data(self, uuid):
         tool = getUtility(IContactImagesTool)
         return tool.create(uuid)
+
+
+class ContactElementView(BrowserView):
+    """ Contact object element view usable below content main viewlet """
+
+    def has_image(self):
+        context = aq_inner(self.context)
+        try:
+            lead_img = context.image
+        except AttributeError:
+            lead_img = None
+        if lead_img is not None:
+            return True
+        return False
+
+    def has_address_info(self):
+        context = aq_inner(self.context)
+        if context.address or context.city:
+            return True
+        return False
+
+    def has_position_info(self):
+        context = aq_inner(self.context)
+        if context.position or context.department:
+            return True
+        return False
+
+    def get_image_data(self, uuid):
+        tool = getUtility(IContactImagesTool)
+        return tool.create(uuid)
